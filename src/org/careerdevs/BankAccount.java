6b153e393b58b76ee0cc09a550ee8617cdb8b680
Edward Dupre <edwd42@gmail.com>
2018-07-05 18:57:46 -0400

    add class BankAccount

---
+++ src/org/careerdevs/BankAccount.java
@@ -0,0 +1,20 @@
+package org.careerdevs;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+
+		BankAccount account = new BankAccount();
+		
+		
+	}
+	
+	public BankAccount() {
+		
+		String customerName;
+		String customerId;
+
+		
+	}
+
+}
